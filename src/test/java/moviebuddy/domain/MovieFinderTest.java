d4b5cb7f43fea57749e18284d4a7411d16559382
iamkimlog <iamkimlog@gmail.com>
2023-09-08 09:32:46 +0900

    04_03 영화검색 컴포넌트 테스트를 JUnit 기반으로 변경

--- src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -1,5 +1,8 @@
 package moviebuddy.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.List;
 
 /**
@@ -7,21 +10,19 @@ import java.util.List;
  */
 public class MovieFinderTest {
 
-	public static void main(String[] args) {
-		MovieBuddyFactory movieBuddyFactory = new MovieBuddyFactory();
-		MovieFinder movieFinder = movieBuddyFactory.movieFinder();
+	MovieBuddyFactory movieBuddyFactory = new MovieBuddyFactory();
+	MovieFinder movieFinder = movieBuddyFactory.movieFinder();
 
-		List<Movie> result = movieFinder.directedBy("Michael Bay");
-		assertEquals(3, result.size());
+	@Test
+	void NotEmpty_directedBy() {
+		List<Movie> movies = movieFinder.directedBy("Michael Bay");
+		Assertions.assertEquals(3, movies.size());
+	}
 
-        result = movieFinder.releasedYearBy(2015);
-        assertEquals(225, result.size());
+	@Test
+	void NotEmpty_releasedYearBy() {
+		List<Movie> movies = movieFinder.releasedYearBy(2015);
+		Assertions.assertEquals(225, movies.size());
 	}
-	
-	static void assertEquals(long expected, long actual) {
-		if (expected != actual) {
-			throw new RuntimeException(String.format("actual(%d) is different from the expected(%d)", actual, expected));			
-		}
-	}
-	
+
 }
