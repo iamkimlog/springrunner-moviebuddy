729387e6d1cb252bfa09124343da193151ec8d79
iamkimlog <iamkimlog@gmail.com>
2023-09-08 14:55:28 +0900

    06_03 자바 기반 빈 구성정보 조합하기

--- src/main/java/moviebuddy/domain/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/domain/MovieBuddyFactory.java
@@ -3,19 +3,27 @@ package moviebuddy.domain;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@Import({ MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class })
 public class MovieBuddyFactory {
 
-    @Bean
-    public MovieReader movieReader() {
-        return new CsvMovieReader();
+    @Configuration
+    static class DomainModuleConfig {
+        @Bean
+        public MovieFinder movieFinder(MovieReader movieReader) {
+            return new MovieFinder(movieReader);
+        }
     }
 
-    @Bean
-    public MovieFinder movieFinder(MovieReader movieReader) {
-        return new MovieFinder(movieReader);
+    @Configuration
+    static class DataSourceModuleConfig {
+        @Bean
+        public MovieReader movieReader() {
+            return new CsvMovieReader();
+        }
     }
 
 }
